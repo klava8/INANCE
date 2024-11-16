057dde2a5a0aa2ebc224b65b9f0152d6b19fe5d1
klava8 <klimasaus1@gmail.com>
2024-11-17 00:58:40 +0300

    Новая концепция сайта

--- restaurant/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import AdditionalInformation
-
-# Register your models here.
-@admin.register(AdditionalInformation)
-class AdditionalInformationAdmin(admin.ModelAdmin):
-    list_display = ('key', 'value')
